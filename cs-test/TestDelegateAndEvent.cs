ba4bda6b5e57775083b7da24547f8c6c97ffdec2
rain_zhao_001 <rangeree@gmail.com>
2020-07-23 15:12:22 +0800

    first commit

---
+++ cs-test/TestDelegateAndEvent.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace TestDelegateAndEvent
+{
+
+    class TestDelagateAndEvent
+    {
+
+        public static void innerStaticSayHello(String name)
+        {
+            Console.WriteLine($"innerStaticSayHello:hello {name}");
+        }
+
+        public void innerSayHello(String name)
+        {
+            Console.WriteLine($"innerSayHello:hello {name}");
+        }
+
+        /*
+         * 声明delegate 
+         */
+        public delegate void SayHelloDelegate(String name);
+
+        /*
+         * 声明delegate对象
+         */
+        public SayHelloDelegate sayHelloDelegate;
+
+        public event SayHelloDelegate sayHeloEvent;
+
+        public void activateSayHelloDelegate(String name)
+        {
+            sayHelloDelegate?.Invoke(name);
+        }
+
+        public void activateSayHelloEvent(String name)
+        {
+            sayHeloEvent?.Invoke(name);
+        }
+
+
+    }
+    class Test
+    {
+        public static void outerSayHello(String name)
+        {
+            Console.WriteLine($"outerSayHello:hello {name}");
+        }
+        static void Main(string[] args)
+        {
+            TestDelagateAndEvent obj = new TestDelagateAndEvent();
+
+            /*
+             * test delagate
+             */
+            //delegate 能使用 "="
+            obj.sayHelloDelegate = obj.innerSayHello;
+            obj.sayHelloDelegate += TestDelagateAndEvent.innerStaticSayHello;
+            obj.sayHelloDelegate += Test.outerSayHello;
+            obj.sayHelloDelegate += (name) =>
+            {
+                Console.WriteLine($"Lambda:hello {name}");
+            };
+            //delegate 能作为对象直接调用invoke
+            Console.WriteLine("delegate 直接invoke");
+            obj.sayHelloDelegate("小明");
+            Console.WriteLine("delegate 在对象内部invoke");
+            obj.activateSayHelloDelegate("小明");
+
+            /*
+             * test event
+             */
+            //event事件只能使用"+="或"-="
+            //test.sayHeloEvent = test.innerSayHello;
+            obj.sayHeloEvent += TestDelagateAndEvent.innerStaticSayHello;
+            obj.sayHeloEvent += Test.outerSayHello;
+            obj.sayHeloEvent += (name) =>
+            {
+                Console.WriteLine($"Lambda:hello {name}");
+            };
+            //event 只能在对象内部invoke
+            //test.sayHeloEvent("小明");
+            Console.WriteLine("event 在对象内部invoke");
+            obj.activateSayHelloEvent("小明");
+
+
+
+        }
+    }
+}
---
+++ test-event/TestEvent.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestEvent
+{
+    /*
+     * 自定义event 事件
+     */
+    public class ShapeEventArgs : EventArgs
+    {
+        public double NewArea
+        {
+            get;
+        }
+
+        public ShapeEventArgs(double area)
+        {
+            this.NewArea = area;
+        }
+    }
+
+    /*
+     * shape抽象类
+     */
+    public abstract class Shape
+    {
+
+        protected double _area;
+        public double Area
+        {
+            get => _area;
+            set => _area = value;
+        }
+
+        public event EventHandler<ShapeEventArgs> ShapeChanged;
+
+        public abstract void Draw();
+
+        protected virtual void OnShapeChange(ShapeEventArgs e)
+        {
+            ShapeChanged?.Invoke(this, e);
+        }
+
+    }
+
+    public class Circle : Shape
+    {
+
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+            _area = 3.14 * _radius * _radius;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Write a Circle!");
+        }
+
+        protected override void OnShapeChange(ShapeEventArgs e)
+        {
+            base.OnShapeChange(e);
+        }
+
+        public void Update(double d)
+        {
+            _radius = d;
+            _area = 3.14 * _radius * _radius;
+            OnShapeChange(new ShapeEventArgs(_area));
+        }
+
+    }
+
+    public class Rectangle : Shape
+    {
+        private double _length;
+        private double _width;
+
+        public Rectangle(double length, double width)
+        {
+            _length = length;
+            _width = width;
+            _area = _length * _width;
+        }
+
+
+        public override void Draw()
+        {
+            Console.WriteLine("Write a Rectangle!");
+        }
+
+        protected override void OnShapeChange(ShapeEventArgs e)
+        {
+            base.OnShapeChange(e);
+        }
+
+        public void Update(double length,double width)
+        {
+            _length = length;
+            _width = width;
+            _area = _length * _width;
+            OnShapeChange(new ShapeEventArgs(_area));
+        }
+    }
+
+    public class ShapeContainer
+    {
+        private readonly  IList<Shape> _list;
+
+        public ShapeContainer()
+        {
+            _list = new List<Shape>();
+        }
+
+        public void AddShape(Shape shape)
+        {
+            _list.Add(shape);
+            shape.ShapeChanged += handleShapeChanged;
+         
+        }
+
+        private void handleShapeChanged(object sender, ShapeEventArgs e)
+        {
+            if (sender is Shape shape)
+            {
+                Console.WriteLine($"Received event. Shape area is now {e.NewArea}");
+                shape.Draw();
+            }
+            
+        }
+
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            ShapeContainer container = new ShapeContainer();
+            Circle circle = new Circle(54);
+            Rectangle rectangle = new Rectangle(12, 9);
+            container.AddShape(circle);
+            container.AddShape(rectangle);
+
+            // Cause some events to be raised.
+            circle.Update(57);
+            rectangle.Update(7, 7);
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
